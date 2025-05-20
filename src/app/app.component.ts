7ae8f3b831566a6961b05656c685e87847024dfb
Marvin Schön <marvin.schoen89@gmail.com>
2025-05-20 18:28:34 +0200

    chore: dev title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from "./toolbar/toolbar.component";
 import { Store } from '@ngrx/store';
 import { loadSongFiles } from './store/song-file.actions';
+import { environment } from '../environment/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +13,10 @@ import { loadSongFiles } from './store/song-file.actions';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'mulu';
-
-  constructor(readonly store: Store) {
-  }
+  constructor(private titleService: Title, readonly store: Store) {}
 
   ngOnInit(): void {
+    this.titleService.setTitle(environment.title);
     this.store.dispatch(loadSongFiles())
   }
 }
