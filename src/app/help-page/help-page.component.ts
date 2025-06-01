b1f6c4d0eaf7492d616342ea3bfd413395896281
Marvin Schön <marvin.schoen89@gmail.com>
2025-06-01 13:58:47 +0200

    feat: help page written

--- src/app/help-page/help-page.component.ts
+++ src/app/help-page/help-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-help-page',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './help-page.component.html',
   styleUrl: './help-page.component.scss'
 })
