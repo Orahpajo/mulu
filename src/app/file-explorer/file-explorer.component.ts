8ed621bfe02af7995e1252c564d45d983bf07362
Marvin Schön <marvin.schoen89@gmail.com>
2025-06-02 15:15:12 +0200

    fix: tree nodes update correctly

--- src/app/file-explorer/file-explorer.component.ts
+++ src/app/file-explorer/file-explorer.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Store } from '@ngrx/store';
 import { closeCurrentSongFile, createSongFile, openSongFile } from '../store/song-file.actions';
 import { selectSongFiles, selectSongTreeNodes } from '../store/song-file.feature';
 import { Router, RouterModule } from '@angular/router';
-import { combineLatest, find, map, Observable } from 'rxjs';
+import { combineLatest, find, map, Observable, Subject, takeUntil, timeout } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
-import {MatTreeModule} from '@angular/material/tree';
+import {MatTreeModule, MatTreeNestedDataSource} from '@angular/material/tree';
 import { SongTreeNode } from '../model/song-tree-node';
+import { SongFile } from '../model/song-file.model';
+import { ViewChild } from '@angular/core';
+import { MatTree } from '@angular/material/tree';
+import { NestedTreeControl } from '@angular/cdk/tree';
 
 
 @Component({
@@ -25,21 +29,40 @@ import { SongTreeNode } from '../model/song-tree-node';
   templateUrl: './file-explorer.component.html',
   styleUrl: './file-explorer.component.scss',
 })
-export class FileExplorerComponent {
+export class FileExplorerComponent implements OnInit, OnDestroy {
 
   @Input()
   sidenavOpen: boolean;
   @Output()
   sidenavOpenChange = new EventEmitter<boolean>();
 
-  songs$= this.store.select(selectSongFiles);
-  treeNodes$ = this.store.select(selectSongTreeNodes);
+  private destroy$ = new Subject<void>();
+
+  songs: SongFile[];
+
+  dataSource = new MatTreeNestedDataSource<SongTreeNode>();
 
   childrenAccessor = (node: SongTreeNode) => node.children ?? [];
   hasChild = (_: number, node: SongTreeNode) => !!node.children && node.children.length > 0;
 
 
   constructor(private store: Store, private router: Router) {}
+
+  ngOnInit(): void {
+    this.store.select(selectSongFiles)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(songs => this.songs = songs);
+    this.store.select(selectSongTreeNodes)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(treeNodes => {
+        this.dataSource.data = treeNodes;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   newSong() {
     this.store.dispatch(createSongFile());
@@ -60,10 +83,9 @@ export class FileExplorerComponent {
   }
 
   songNameById(songId: string) {
-    return this.songs$.pipe(
-      map(songs => songs.find(song => song.id === songId) ?? {name: 'ERROR '+songId}),
-      map(song => song.name)
-    );
+    return this.songs
+      .find(song => song.id === songId)
+      ?.name ?? 'ERROR ' + songId;
   }
   
 }
