b55d9281a7628a20e0f124e73d536c2812efb63e
Marvin Schön <marvin.schoen89@gmail.com>
2025-06-10 23:06:30 +0200

    content: face rückansicht aktualisiert.

--- src/app/services/common-song.service.ts
+++ src/app/services/common-song.service.ts
@@ -21,7 +21,7 @@ export class CommonSongService {
     { name: 'I_5 Tratsch.mulu', lastEdit: new Date(2025, 6, 10, 17, 0) },
     { name: 'I_12 Schaff die Männer ran.mulu', lastEdit: new Date(2025, 6, 10, 17, 0) },
     { name: 'II_24 Mörder Mörder.mulu', lastEdit: new Date(2025, 6, 10, 17, 0) },
-    { name: 'I_3 Facade - Choreo.mulu', lastEdit: new Date(2025, 6, 10, 17, 0) },
+    { name: 'I_3 Facade - Choreo.mulu', lastEdit: new Date(2025, 6, 10, 23, 0) },
   ];
 
   constructor(private http: HttpClient, private store: Store) { }
