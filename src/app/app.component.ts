e8dee624a6635b1363a75dc764e32ecbefb17bd2
Marvin Schön <marvin.schoen89@gmail.com>
2025-06-10 23:30:15 +0200

    fix: navigate home on reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from "./toolbar/toolbar.component";
 import { Store } from '@ngrx/store';
-import { loadSongFiles } from './store/song-file.actions';
+import { closeCurrentSongFile, loadSongFiles } from './store/song-file.actions';
 import { environment } from '../environments/environment';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { version } from '../environments/version'
@@ -19,10 +19,13 @@ export class AppComponent implements OnInit {
   appVersion = version;
   sidenavOpen = false;
 
-  constructor(private titleService: Title, readonly store: Store) {}
+  constructor(private titleService: Title, readonly store: Store, readonly router: Router) {}
 
   ngOnInit(): void {
     this.titleService.setTitle(environment.title);
     this.store.dispatch(loadSongFiles())
+    // close current songfile in reload
+    this.store.dispatch(closeCurrentSongFile());
+    this.router.navigate(['/']);
   }
 }
