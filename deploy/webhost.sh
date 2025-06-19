10a391242189ffc753e24898dbc47761a97eb9fc
Marvin Schön <marvin.schoen89@gmail.com>
2025-06-19 13:14:54 +0200

    fix: dont deploy common songs

--- deploy/webhost.sh
+++ deploy/webhost.sh
@@ -22,7 +22,6 @@ ng build --configuration $BUILD_CONFIG
 # move htaccess files to dist
 cp "$SCRIPT_DIR/htaccess/.htaccess" dist/mulu/browser/
 cp "$SCRIPT_DIR/htaccess/.htpasswd" dist/mulu/browser/
-mkdir -p dist/mulu/browser/commonSongs
 
 # read Credentials from credentials.txt
 CRED_FILE="$SCRIPT_DIR/credentials.txt"
